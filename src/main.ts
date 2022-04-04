a65cd48567c30df0f59d3b00a4f7cad130ad05a0
dvortsevoyai <dvortsevoyai@gmail.com>
2022-04-04 17:09:11 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as $ from 'jquery';
+
+(window as any)['$'] = $;
+
 if (environment.production) {
   enableProdMode();
 }
